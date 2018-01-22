14edf9d6763e6976d0ac0a48ff7b8cb877e74ad5
Pierre_T <pierre.tsiakkaros@gmail.com>
2018-01-22 21:05:45 +0100

    dynamic and responsive menu

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,67 @@
 <template>
   <v-app>
-     <v-toolbar dark class="deep-orange darken-4">
-      <v-toolbar-side-icon   class="hidden-sm-and-up" @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+    <v-toolbar dark class="deep-orange darken-4">
+      <v-toolbar-side-icon class="hidden-sm-and-up" @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
       <v-toolbar-title>My Post App</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-item class="hidden-xs-only">
-         <v-btn flat>
-        <v-icon>view_list</v-icon>
-        View Posts
-        </v-btn>
-      <v-btn flat>
-        <v-icon>create</v-icon>
-        Create Post</v-btn>
-      </v-toolbar-item>
+      <span class="hidden-xs-only" v-for="item in menuItems" :key="item.title">
+           <v-btn flat>
+          <v-icon>{{item.icon}}</v-icon>
+          {{item.title}}
+          </v-btn>
+       
+        </span>
     </v-toolbar>
-    <v-navigation-drawer v-model="sideNav"
-    class="hidden-sm-and-up">
+    <v-navigation-drawer v-model="sideNav" class="hidden-sm-and-up">
       <v-list>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>view_list</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            View Posts
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>create</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            Create Post
-          </v-list-tile-content>
+          <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-btn flat>
+            <v-list-tile-action>
+              <v-icon>{{item.icon}}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              {{item.title}}
+            </v-list-tile-content>
+          </v-btn>
         </v-list-tile>
       </v-list>
-
+  
     </v-navigation-drawer>
-   
+  
     <main>
-
+  
     </main>
   </v-app>
- </template>
+</template>
 
 <script>
-export default {
-  data() {
-    return {
-      sideNav: false
-    };
-  },
-  name: "App"
-};
+  export default {
+    data() {
+      return {
+        sideNav: false,
+        menuItems: [{
+            icon: "view_list",
+            title: "View Posts"
+          },
+          {
+            icon: "create",
+            title: "New Post"
+          },
+          {
+            icon: "person",
+            title: "Profile"
+          },
+          {
+            icon: "face",
+            title: "Sign Up"
+          },
+          {
+            icon: "lock_open",
+            title: "Sign In"
+          }
+        ]
+      };
+    },
+    name: "App"
+  };
 </script>
