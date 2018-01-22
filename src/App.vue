d64f90f08e8b4e377519446447fc8ea430046c74
Pierre_T <pierre.tsiakkaros@gmail.com>
2018-01-23 00:12:36 +0100

    caroussel OK

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
   <v-app>
     <v-toolbar dark class="deep-orange darken-4">
       <v-toolbar-side-icon class="hidden-sm-and-up" @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
-      <v-toolbar-title>My Post App</v-toolbar-title>
+      <v-toolbar-title ><router-link tag="span" style="cursor : pointer" to="/"> My Post App</router-link> </v-toolbar-title>
       <v-spacer></v-spacer>
       <span class="hidden-xs-only" v-for="item in menuItems" :key="item.title">
-           <v-btn flat>
+           <v-btn flat router :to="item.link">
           <v-icon>{{item.icon}}</v-icon>
           {{item.title}}
           </v-btn>
        
         </span>
     </v-toolbar>
-    <v-navigation-drawer v-model="sideNav" class="hidden-sm-and-up">
+    <v-navigation-drawer fixed temporary light v-model="sideNav" class="hidden-sm-and-up">
       <v-list>
           <v-list-tile v-for="item in menuItems" :key="item.title">
-          <v-btn flat>
+          <v-btn  router :to="item.link" flat style="width:100%">
             <v-list-tile-action>
               <v-icon>{{item.icon}}</v-icon>
             </v-list-tile-action>
@@ -41,23 +41,28 @@
         sideNav: false,
         menuItems: [{
             icon: "view_list",
-            title: "View Posts"
+            title: "View Posts",
+            link:'/posts'
           },
           {
             icon: "create",
-            title: "New Post"
+            title: "New Post",
+            link:'/createpost'
           },
           {
             icon: "person",
-            title: "Profile"
+            title: "Profile",
+            link:'/profile'
           },
           {
             icon: "face",
-            title: "Sign Up"
+            title: "Sign Up",
+            link:'/signup'
           },
           {
             icon: "lock_open",
-            title: "Sign In"
+            title: "Sign In",
+            link:'/signin'
           }
         ]
       };
@@ -65,3 +70,14 @@
     name: "App"
   };
 </script>
+
+<style scoped>
+  main{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: space-between;
+  }
+
+</style>
+
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-      <p>Home Vue works !</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-sm-right text-xs-center">
+        <v-btn class="orange lighten-2" large router to="/posts">See All Posts</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-sm-left text-xs-center">
+        <v-btn class="orange lighten-2" large router to="/createpost">Write New Post</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="post in posts" v-bind:src="post.imgUrl" :key="post.id">
+            <div class="title">{{post.title}}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        posts: [{
+            imgUrl: 'http://lorempixel.com/output/nature-q-c-785-376-1.jpg',
+            id: 'qsjhdqshdqshdkjqd',
+            title: 'My First Post'
+  
+          },
+          {
+            imgUrl: 'http://lorempixel.com/output/nature-q-c-785-376-6.jpg',
+            id: 'hdqshdkjqshdkqj',
+            title: 'My Second Post'
+  
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style <style scoped>
+  v-carousel-item {
+    position: relative;
+  }
+  
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 10px;
+    width:100%;
+    text-align: center;
+  }
+</style>
+
